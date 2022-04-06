511794423a0bf10f5416caa6a2d871d5ba942c94
Shyby Mallanveliyil <shybyhamza@gmail.com>
2022-04-05 23:34:35 -0500

    Little bit more

---
+++ src/shared/events.js
@@ -0,0 +1,46 @@
+export const EVENTS = [
+    {
+        id: 0,
+        name: "Art in the Park",
+        image: "/assets/images/Event4.jpg",
+        elevation: 1233,
+        featured: true,
+        time: "3 - 5 pm",
+        date: "July 5",
+        venue: "Cedar Park",
+        description: "Come join us for the funfilled event 'Art in the Park' at Cedar park. Ms.Eileen from 'Art Kids' will show our young artists how to make paper flowers. "
+    },
+    {
+        id: 1,
+        name: "Movie in the Park",
+        image: "/assets/images/Event3.JPG",
+        elevation: 700,
+        featured: true,
+        time: "6pm",
+        date: "June 29",
+        venue: "Berger Park",
+        description: "Lilo and Stitch"
+    },
+    {
+        id: 2,
+        name: "Farmers Market",
+        image: "/assets/images/Event4.jpg",
+        elevation: 1233,
+        featured: true,
+        time: "10 - 4 pm",
+        date: "June 13,14 and 15",
+        venue: "Between Clark and Ashland at Gregory",
+        description: "Locally grown fresh seasonal produce and prepared foods directly from growers."
+    },
+    {
+        id: 3,
+        name: "Edgewater Art Festival",
+        image: "/assets/images/Event4.jpg",
+        elevation: 1233,
+        featured: false,
+        time: "10 - 3 pm",
+        date: "June 20 and 21",
+        venue: "Between Sheridan and Broadway at Granville",
+        description: "Unique handmade products are sold by our local artists."
+    }
+];
